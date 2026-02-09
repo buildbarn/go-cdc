12f671e48d903a4e3dca01cd5095098a8b0d0c62
Ed Schouten <eschouten@apple.com>
2026-02-09 21:39:28 +0100

    Add subtests for RepMaxCDC for each horizon size

--- rep_max_content_defined_chunker_test.go
+++ rep_max_content_defined_chunker_test.go
@@ -2,6 +2,7 @@ package cdc_test
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"math/rand"
 	"testing"
@@ -18,34 +19,36 @@ func TestRepMaxContentDefinedChunker(t *testing.T) {
 	r2 := rand.New(rand.NewSource(seed))
 
 	for horizonSizeBytes := 0; horizonSizeBytes <= 16*1024; horizonSizeBytes += 2 * 1024 {
-		for i := 0; i < 100; i++ {
-			chunker1 := cdc.NewSimpleRepMaxContentDefinedChunker(
-				bufio.NewReaderSize(io.LimitReader(r1, 1024*1024), 64*1024),
-				/* minSizeBytes = */ 2*1024,
-				horizonSizeBytes,
-			)
-			chunker2 := cdc.NewRepMaxContentDefinedChunker(
-				bufio.NewReaderSize(io.LimitReader(r2, 1024*1024), 64*1024),
-				/* minSizeBytes = */ 2*1024,
-				horizonSizeBytes,
-			)
+		t.Run(fmt.Sprintf("Horizon=%d", horizonSizeBytes), func(t *testing.T) {
+			for i := 0; i < 100; i++ {
+				chunker1 := cdc.NewSimpleRepMaxContentDefinedChunker(
+					bufio.NewReaderSize(io.LimitReader(r1, 1024*1024), 64*1024),
+					/* minSizeBytes = */ 2*1024,
+					horizonSizeBytes,
+				)
+				chunker2 := cdc.NewRepMaxContentDefinedChunker(
+					bufio.NewReaderSize(io.LimitReader(r2, 1024*1024), 64*1024),
+					/* minSizeBytes = */ 2*1024,
+					horizonSizeBytes,
+				)
 
-			for totalRead := 0; totalRead < 1024*1024; {
-				chunk1, err1 := chunker1.ReadNextChunk()
-				require.NoError(t, err1)
-				require.LessOrEqual(t, 2*1024, len(chunk1))
-				require.Greater(t, 4*1024, len(chunk1))
+				for totalRead := 0; totalRead < 1024*1024; {
+					chunk1, err1 := chunker1.ReadNextChunk()
+					require.NoError(t, err1)
+					require.LessOrEqual(t, 2*1024, len(chunk1))
+					require.Greater(t, 4*1024, len(chunk1))
 
-				chunk2, err2 := chunker2.ReadNextChunk()
-				require.NoError(t, err2)
-				require.Equal(t, chunk1, chunk2)
-				totalRead += len(chunk1)
+					chunk2, err2 := chunker2.ReadNextChunk()
+					require.NoError(t, err2)
+					require.Equal(t, chunk1, chunk2)
+					totalRead += len(chunk1)
+				}
+
+				_, err1 := chunker1.ReadNextChunk()
+				require.Equal(t, io.EOF, err1)
+				_, err2 := chunker2.ReadNextChunk()
+				require.Equal(t, io.EOF, err2)
 			}
-
-			_, err1 := chunker1.ReadNextChunk()
-			require.Equal(t, io.EOF, err1)
-			_, err2 := chunker2.ReadNextChunk()
-			require.Equal(t, io.EOF, err2)
-		}
+		})
 	}
 }
