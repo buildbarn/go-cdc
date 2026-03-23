4ee4ff38b8346acdb1182e1cab8d491af9a943e1
Ed Schouten <eschouten@apple.com>
2026-03-23 20:38:25 +0100

    Decompose stats simulation and printing into two separate tools
    This allows us to run the simulation continously and later make tweaks
    to the approximation functions.

---
+++ cmd/print_maxcdc_stats/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"log"
+	"math"
+	"os"
+)
+
+func main() {
+	const buckets = 1000
+	for minSize := buckets - 1; minSize >= 1; minSize-- {
+		samplesFile := fmt.Sprintf("../simulate_maxcdc/samples/%d/%d", buckets, minSize)
+		existingSamples, err := os.ReadFile(samplesFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if len(existingSamples) != 8*buckets {
+			log.Fatal("Invalid samples length")
+		}
+		var samples [buckets]uint64
+		for i := 0; i < buckets; i++ {
+			samples[i] = binary.LittleEndian.Uint64(existingSamples[8*i:])
+		}
+
+		maxSize := minSize + buckets - 1
+		r := float64(maxSize) / float64(minSize)
+		basicAverage := float64(minSize+maxSize) / 2
+
+		iterations := uint64(0)
+		totalSize := uint64(0)
+		coveredSize := uint64(0)
+		totalSizeSize := uint64(0)
+		for i, s := range samples {
+			chunkSize := uint64(minSize + i)
+			iterations += s
+			totalSize += s * chunkSize
+			coveredSize += s * min(chunkSize, uint64(maxSize-minSize+1))
+			totalSizeSize += s * chunkSize * chunkSize
+		}
+
+		plateauSamples := samples[len(samples)-minSize:]
+		plateauTotalSamples := uint64(0)
+		for _, s := range plateauSamples {
+			plateauTotalSamples += s
+		}
+		realPlateauProbability := float64(plateauTotalSamples) / float64(len(plateauSamples)) / float64(samples[0])
+		expectedPlateauProbability := (2.289 + 0.577*r - 3.053*r*r) / (1 - 2.472*r - 1.179*r*r)
+
+		realSkewOut := (float64(totalSize)/float64(iterations))/basicAverage - 1
+		expectedSkewOut := 0.1449 - 1.162/math.Pow(r+3.015, 1.274)
+
+		realSkewModified := (float64(totalSizeSize)/float64(totalSize))/basicAverage - 1
+		expectedSkewModified := 0.4275 - 0.245*math.Pow(r, -0.40) - 0.373*math.Pow(r, -1.55)
+
+		realCoverage := float64(coveredSize) / float64(totalSize)
+		expectedCoverage := math.Pow(r, 2.75) / (math.Pow(r, 2.75) + math.Pow(2, 2.75-1))
+
+		fmt.Printf(
+			"%f, %f,%f,%f, %f,%f,%f, %f,%f,%f, %f,%f,%f\n",
+			r,
+			realPlateauProbability,
+			expectedPlateauProbability,
+			realPlateauProbability/expectedPlateauProbability,
+			realSkewOut,
+			expectedSkewOut,
+			realSkewOut/expectedSkewOut,
+			realSkewModified,
+			expectedSkewModified,
+			realSkewModified/expectedSkewModified,
+			realCoverage,
+			expectedCoverage,
+			realCoverage/expectedCoverage,
+		)
+	}
+}
--- cmd/simulate_maxcdc/main.go
+++ cmd/simulate_maxcdc/main.go
@@ -3,97 +3,77 @@ package main
 import (
 	"context"
 	crypto_rand "crypto/rand"
+	"encoding/binary"
 	"fmt"
-	"math"
+	"log"
 	"math/rand/v2"
+	"os"
 	"runtime"
-	"sync"
 
 	"golang.org/x/sync/semaphore"
 )
 
 func main() {
-	var wg sync.WaitGroup
 	concurrency := semaphore.NewWeighted(int64(runtime.NumCPU()))
+	for {
+		const buckets = 1000
+		for minSizeIter := 1; minSizeIter < buckets; minSizeIter++ {
+			minSize := minSizeIter
 
-	const buckets = 1000
-	for minSizeIter := 1; minSizeIter < buckets; minSizeIter++ {
-		minSize := minSizeIter
+			concurrency.Acquire(context.Background(), 1)
+			go func() {
+				defer concurrency.Release(1)
 
-		concurrency.Acquire(context.Background(), 1)
-		wg.Add(1)
-		go func() {
-			defer concurrency.Release(1)
-			defer wg.Done()
+				var seed [32]byte
+				crypto_rand.Read(seed[:])
+				rng := rand.NewChaCha8(seed)
 
-			var seed [32]byte
-			crypto_rand.Read(seed[:])
-			rng := rand.NewChaCha8(seed)
+				// Perform simulation.
+				var hashes [buckets]uint64
+				hashesLength := 0
+				var samples [buckets]uint64
+				for range 10000000 {
+					for hashesLength < len(hashes) {
+						hashes[hashesLength] = rng.Uint64()
+						hashesLength++
+					}
 
-			var samples [buckets]int
-			const iterations = 1000000
-			totalSize := 0
-			uncoveredSize := 0
-			var hashes [buckets]uint64
-			hashesLength := 0
-			for range iterations {
-				for hashesLength < len(hashes) {
-					hashes[hashesLength] = rng.Uint64()
-					hashesLength++
-				}
-
-				bestIndex := 0
-				bestHash := hashes[bestIndex]
-				for i := 1; i < buckets; i++ {
-					if hash := hashes[i]; bestHash < hash {
-						bestIndex = i
-						bestHash = hash
+					bestIndex := 0
+					bestHash := hashes[bestIndex]
+					for i := 1; i < buckets; i++ {
+						if hash := hashes[i]; bestHash < hash {
+							bestIndex = i
+							bestHash = hash
+						}
+					}
+					samples[bestIndex]++
+					if start := bestIndex + minSize; start >= len(hashes) {
+						hashesLength = 0
+					} else {
+						hashesLength = copy(hashes[:], hashes[bestIndex+minSize:])
 					}
 				}
-				samples[bestIndex]++
-				totalSize += minSize + bestIndex
 
-				if start := bestIndex + minSize; start >= len(hashes) {
-					hashesLength = 0
-					uncoveredSize += start - len(hashes)
-				} else {
-					hashesLength = copy(hashes[:], hashes[bestIndex+minSize:])
+				// Reload existing sample counts.
+				samplesFile := fmt.Sprintf("samples/%d/%d", buckets, minSize)
+				if existingSamples, _ := os.ReadFile(samplesFile); len(existingSamples) == 8*buckets {
+					for i := 0; i < buckets; i++ {
+						samples[i] += binary.LittleEndian.Uint64(existingSamples[8*i:])
+					}
 				}
-			}
 
-			maxSize := minSize + buckets - 1
-			r := float64(maxSize) / float64(minSize)
-
-			plateauSamples := samples[len(samples)-minSize:]
-			plateauTotalSamples := 0
-			for _, s := range plateauSamples {
-				plateauTotalSamples += s
-			}
-			realPlateauProbability := float64(plateauTotalSamples) / float64(len(plateauSamples)) / float64(samples[0])
-			expectedPlateauProbability := (2.289 + 0.577*r - 3.053*r*r) / (1 - 2.472*r - 1.179*r*r)
-
-			realSkew := (float64(totalSize)/iterations)/(float64(minSize+maxSize)/2) - 1
-			expectedSkew := 0.1449 - 1.162/math.Pow(r+3.015, 1.274)
-
-			realCoverage := float64(totalSize-uncoveredSize) / float64(totalSize)
-			expectedCoverage := math.Pow(r, 2.75) / (math.Pow(r, 2.75) + math.Pow(2, 2.75-1))
-
-			fmt.Printf(
-				"%d,%d,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n",
-				minSize,
-				maxSize,
-				r,
-				realPlateauProbability,
-				expectedPlateauProbability,
-				realPlateauProbability-expectedPlateauProbability,
-				realSkew,
-				expectedSkew,
-				realSkew-expectedSkew,
-				realCoverage,
-				expectedCoverage,
-				realCoverage-expectedCoverage,
-			)
-		}()
+				// Write new sample counts to disk.
+				var newSamples [8 * buckets]byte
+				for i := 0; i < buckets; i++ {
+					binary.LittleEndian.PutUint64(newSamples[8*i:], samples[i])
+				}
+				if err := os.WriteFile(samplesFile+".tmp", newSamples[:], 0o666); err != nil {
+					log.Fatal(err)
+				}
+				if err := os.Rename(samplesFile+".tmp", samplesFile); err != nil {
+					log.Fatal(err)
+				}
+			}()
+		}
 	}
-	wg.Wait()
 }
